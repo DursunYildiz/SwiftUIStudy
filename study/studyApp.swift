197c09d7d0576186586af56592a9650959d32641
Dursun Yıldız <duyildiz@innova.com.tr>
2023-07-30 10:52:43 +0300

    Initial Commit

---
+++ study/studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  studyApp.swift
+//  study
+//
+//  Created by Dursun YILDIZ on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
